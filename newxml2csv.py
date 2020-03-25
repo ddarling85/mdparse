a30fdd6e229c165c65629321f2ef4ee6b3908936
Automation User <automate@hv-centos2.paraklesys-inc.com>
2020-03-25 10:25:45 -0400

    Updating and adding files

--- newxml2csv.py
+++ newxml2csv.py
@@ -2,9 +2,11 @@ import xml.etree.ElementTree as ET
 import csv
 import logging
 
+#tree = ET.parse("resident.xml")
 tree = ET.parse("metatest.xml")
 root = tree.getroot()
-logging.basicConfig(filename='example.log',level=logging.DEBUG)
+#root = ET.fromstring(tree)
+logging.basicConfig(filename='metatest.log',level=logging.DEBUG)
 
 # open a file for writing
 
@@ -17,8 +19,18 @@ resident_head = []
 
 count = 0
 
-for member in root.iter('{urn:oasis:names:tc:SAML:2.0:metadata}EntityDescriptor'): 
-   print member.attrib
+for member in root.findall():
+#for member in root:
+
+   testEmnt = member.find('{urn:oasis:names:tc:SAML:2.0:metadata}EntityDescriptor')
+#   print(member.find('{urn:oasis:names:tc:SAML:2.0:metadata}SPSSODescriptor'))
+#   print(root.tag, root.attrib)
+#   print(root, root.tag, root.attrib)
+#   print(root)
+#   print(member.tag, member.attrib)
+   print(member.find('entityID'))
+   print(member, member.tag, member.attrib)
+#   print(testEmnt.attrib('entityID').text)
 
 Resident_data.close()
 
