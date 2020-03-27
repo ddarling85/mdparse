7fe4453070eca57075df97c62a0268eaacdad00f
Tomcat Service Account <icam-svc@sso-dev3.csp.noaa.gov>
2020-03-27 18:28:39 -0400

    Updated files

--- newxml2csv.py
+++ newxml2csv.py
@@ -10,7 +10,7 @@ logging.basicConfig(filename='metatest.log',level=logging.DEBUG)
 
 # open a file for writing
 
-Resident_data = open('testxmlfile.csv', 'w')
+Resident_data = open('metatest.csv', 'w')
 
 # create the csv writer object
 
@@ -19,18 +19,33 @@ resident_head = []
 
 count = 0
 
-for member in root.findall():
+#for member in root.iter():
+#for member in root.findall('{urn:oasis:names:tc:SAML:2.0:metadata}EntityDescriptor'):
+for member in root.findall('{urn:oasis:names:tc:SAML:2.0:metadata}ContactPerson'):
 #for member in root:
 
-   testEmnt = member.find('{urn:oasis:names:tc:SAML:2.0:metadata}EntityDescriptor')
+#   testEmnt = member.find("ContactPerson")
+   testEmnt = member.find('{urn:oasis:names:tc:SAML:2.0:metadata}Company')
+#   testEmnt1 = member.findall('{urn:oasis:names:tc:SAML:2.0:metadata}EntityDescriptor')
+#   testEmnt2 = member.attrib.find('entityID')
+   testEmnt2 = member.get('contactType')
 #   print(member.find('{urn:oasis:names:tc:SAML:2.0:metadata}SPSSODescriptor'))
-#   print(root.tag, root.attrib)
 #   print(root, root.tag, root.attrib)
-#   print(root)
+#   print(root.tag, root.attrib)
+#   print(root.attrib)
 #   print(member.tag, member.attrib)
-   print(member.find('entityID'))
+#   print(member.find('entityID'))
    print(member, member.tag, member.attrib)
-#   print(testEmnt.attrib('entityID').text)
+#   print(member.tag, member.attrib)
+#   print(member.attrib)
+#   print(member.tag)
+#   print(member)
+#   print(member[0][0].text)
+#   print(member('entityID').text)
+#   print(testEmnt('entityID').text)
+   print(testEmnt.tag)
+#   print(testEmnt1)
+   print(testEmnt2)
 
 Resident_data.close()
 
